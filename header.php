4b5745dbb3aa2db3e06ede6a07c065819c91e1de
Yosuke Miyajima <mayo.suke@gmail.com>
2017-03-23 11:19:30 +0000

    Move position of the site title

--- header.php
+++ header.php
@@ -12,7 +12,55 @@
 	</head>
 	
 	<body <?php body_class(); ?>>
+			<div class="header section-inner">
 		
+			<?php if ( get_theme_mod( 'hitchcock_logo' ) ) : ?>
+			
+		        <a class="blog-logo" href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'title' ) ); ?> &mdash; <?php echo esc_attr( get_bloginfo( 'description' ) ); ?>' rel='home'>
+		        	<img src='<?php echo esc_url( get_theme_mod( 'hitchcock_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'title' ) ); ?>'>
+		        </a>
+		
+			<?php else : ?>
+		
+				<h1 class="blog-title">
+					<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'title' ) ); ?> &mdash; <?php echo esc_attr( get_bloginfo( 'description' ) ); ?>" rel="home"><?php echo esc_attr( get_bloginfo( 'title' ) ); ?></a>
+				</h1>
+				
+			<?php endif; ?>
+			
+			<?php if ( get_bloginfo('description') ) : ?>
+			
+				<p class="blog-description"><?php echo bloginfo('description'); ?></p>
+			
+			<?php endif; ?>
+			
+			<?php if ( has_nav_menu( 'social' ) ) : ?>
+			
+				<ul class="social-menu">
+							
+					<?php 
+						wp_nav_menu(
+							array(
+								'theme_location'	=>	'social',
+								'container'			=>	'',
+								'container_class'	=>	'menu-social',
+								'items_wrap'		=>	'%3$s',
+								'menu_id'			=>	'menu-social-items',
+								'menu_class'		=>	'menu-items',
+								'depth'				=>	1,
+								'link_before'		=>	'<span class="screen-reader-text">',
+								'link_after'		=>	'</span>',
+								'fallback_cb'		=>	'',
+							)
+						);
+					?>
+					
+				</ul> <!-- /social-menu -->
+			
+			<?php endif; ?>
+			
+		</div> <!-- /header -->	
+
 		<div class="navigation">
 			
 			<div class="section-inner">
@@ -94,51 +142,4 @@
 		
 		<div class="header-image"></div>
 	
-		<div class="header section-inner">
-		
-			<?php if ( get_theme_mod( 'hitchcock_logo' ) ) : ?>
-			
-		        <a class="blog-logo" href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'title' ) ); ?> &mdash; <?php echo esc_attr( get_bloginfo( 'description' ) ); ?>' rel='home'>
-		        	<img src='<?php echo esc_url( get_theme_mod( 'hitchcock_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'title' ) ); ?>'>
-		        </a>
-		
-			<?php else : ?>
-		
-				<h1 class="blog-title">
-					<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'title' ) ); ?> &mdash; <?php echo esc_attr( get_bloginfo( 'description' ) ); ?>" rel="home"><?php echo esc_attr( get_bloginfo( 'title' ) ); ?></a>
-				</h1>
-				
-			<?php endif; ?>
-			
-			<?php if ( get_bloginfo('description') ) : ?>
-			
-				<p class="blog-description"><?php echo bloginfo('description'); ?></p>
-			
-			<?php endif; ?>
-			
-			<?php if ( has_nav_menu( 'social' ) ) : ?>
-			
-				<ul class="social-menu">
-							
-					<?php 
-						wp_nav_menu(
-							array(
-								'theme_location'	=>	'social',
-								'container'			=>	'',
-								'container_class'	=>	'menu-social',
-								'items_wrap'		=>	'%3$s',
-								'menu_id'			=>	'menu-social-items',
-								'menu_class'		=>	'menu-items',
-								'depth'				=>	1,
-								'link_before'		=>	'<span class="screen-reader-text">',
-								'link_after'		=>	'</span>',
-								'fallback_cb'		=>	'',
-							)
-						);
-					?>
-					
-				</ul> <!-- /social-menu -->
-			
-			<?php endif; ?>
-			
-		</div> <!-- /header -->
+
