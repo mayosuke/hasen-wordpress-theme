bb74040897e26b1dcf1b4f44314fb68363308f98
Yosuke Miyajima <mayo.suke@gmail.com>
2017-04-14 20:24:37 +0000

    Disable search so far

--- header.php
+++ header.php
@@ -85,14 +85,14 @@
 						));
 						
 					} ?>
-					
+					<?php /*
 					<li class="header-search">
 						<form method="get" class="search-form" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 							<input type="search" class="search-field" name="s" placeholder="<?php _e('Search Form','hitchcock'); ?>" /> 
 							<a class="search-button" onclick="document.getElementById('search-form').submit(); return false;"><div class="fa fw fa-search"></div></a>
 						</form>
 					</li>
-					
+					*/?>
 				</ul>
 				
 				<div class="clear"></div>
@@ -134,7 +134,7 @@
 						
 					</ul>
 					
-					<?php get_search_form(); ?>
+					<?php //get_search_form(); ?>
 			
 			</div> <!-- /mobile-navigation -->
 			
