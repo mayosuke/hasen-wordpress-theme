02c8400f968b113dcfd7538f276997dc75c78bf8
Yosuke Miyajima <mayo.suke@gmail.com>
2017-04-16 11:46:48 +0000

    Move menu location from page header to page footer

--- footer.php
+++ footer.php
@@ -1,3 +1,45 @@
+		<div class="navigation">
+			
+			<div class="section-inner">
+				
+				<ul class="main-menu">
+																		
+					<?php if ( has_nav_menu( 'primary' ) ) {
+																		
+						wp_nav_menu( array( 
+						
+							'container' => '', 
+							'items_wrap' => '%3$s',
+							'theme_location' => 'primary', 
+														
+						) ); } else {
+					
+						wp_list_pages( array(
+						
+							'container' => '',
+							'title_li' => ''
+						
+						));
+						
+					} ?>
+					<?php /*
+					<li class="header-search">
+						<form method="get" class="search-form" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+							<input type="search" class="search-field" name="s" placeholder="<?php _e('Search Form','hitchcock'); ?>" /> 
+							<a class="search-button" onclick="document.getElementById('search-form').submit(); return false;"><div class="fa fw fa-search"></div></a>
+						</form>
+					</li>
+					*/?>
+				</ul>
+				
+				<div class="clear"></div>
+				
+			</div> <!-- /section-inner -->
+			
+		</div> <!-- /navigation -->
+	
+
+
 <div class="credits section-inner">
 			
 	<p>&copy; <?php echo date('Y'); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo('name'); ?></a></p>
