75b38caf6f9a83ffb0a8074f370127517489b3b8
Yosuke Miyajima <mayo.suke@gmail.com>
2017-04-30 17:44:45 +0000

    Add meta tag for google site verification

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 		
 		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
+    <meta name="google-site-verification" content="tWWKTwekfFjSuZpHNA6LgI1GSkNbOPtOEbL_HY0VeQ8" />
 		 
 		<?php wp_head(); ?>
 	
